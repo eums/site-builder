36635a5f65b19b5d2acf1e77e088f1ab6add5c22
Harry Garrood <harry@garrood.me>
2014-09-23 16:49:54 +0100

    Force https

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 
 class SiteBuilder < Sinatra::Base
+  # force https
+  before do
+    if !request.secure?
+      content_type :json
+      halt json_status 400,
+        "Please use HTTPS: https://#{settings.host}#{request.path}"
+    end
+  end
+
   get '/' do
     'hello from site-builder'
   end
